59a7b84cfde7cc87853aa6664bd7a98bee3525af
阮颖 <hello-world@sjtu.edu.cn>
2021-07-05 14:26:48 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+namespace CShopProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] num = { "Sword", "Gun" };
+            Thread th = Thread.CurrentThread;
+            th.Name = "MainThread";
+            Console.WriteLine("This is {0}", th.Name);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
